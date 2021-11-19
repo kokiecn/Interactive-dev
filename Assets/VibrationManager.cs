25a66c99a27c1aecf59fde8a273a43fe0ad73734
koki <kokik@nangokrstudios.jp>
2021-11-19 21:42:03 +0900

    オブジェクトお衝突してしまったときのブレ挙動おスマホのバイブレーション

--- Assets/VibrationManager.cs
+++ Assets/VibrationManager.cs
@@ -2,32 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public static class VibrationManager
-
+public class VibrationManager : MonoBehaviour
 {
-#if UNITY_ANDROID && !UNITY_EDITOR
-    public static AndroidJavaClass unityPlayer = new AndroidJavaClass("com.unity3d.player.UnityPlayer");
-    public static AndroidJavaObject currentActivity = unityPlayer.GetStatic<AndroidJavaObject>("currentActivity");
-    public static AndroidJavaObject vibrator = currentActivity.Call<AndroidJavaObject>("getSystemService", "vibrator");
-#else
-    public static AndroidJavaClass unityPlayer;
-    public static AndroidJavaObject currentActivity;
-    public static AndroidJavaObject vibrator;
-#endif
+    
+    [SerializeField] private CameraShake _camshake;
 
-    public static void Vibrate(long milliseconds)
+    private void Update()
     {
-        if (isAndroid())
-            vibrator.Call("vibrate", milliseconds);
-        else
-            Handheld.Vibrate();
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _camshake.Shake(0.2f, 0.01f);
+        }
     }
-    private static bool isAndroid()
-    {
-#if UNITY_ANDROID && !UNITY_EDITOR
-            return true;
-#else
-        return false;
-#endif
-    }
+
+
 }
