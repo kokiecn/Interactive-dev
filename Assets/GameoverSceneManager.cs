b7da901801e672a3c3c7df9f4298a953b5e47327
koki <kokik@nangokrstudios.jp>
2021-11-30 12:20:34 +0900

    シーン遷移追加

---
+++ Assets/GameoverSceneManager.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameoverSceneManager : MonoBehaviour
+{
+    [SerializeField] private Button backButton;
+    private void Start()
+    {
+        backButton.onClick.AddListener(() =>
+        {
+            SceneManager.LoadScene("StartScene");
+        });
+
+    }
+}
