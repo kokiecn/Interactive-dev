aa8ee469e4869200a39acf4beb9c6f27d37dffca
koki <kokik@nangokrstudios.jp>
2021-12-05 19:07:01 +0900

    ぐりぐり修正

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -1,21 +1,43 @@
 using UnityEngine;
 public class Test : MonoBehaviour
 {
-    private double _lastCompassUpdateTime = 0;
-    private Quaternion _correction = Quaternion.identity;
-    private Quaternion _targetCorrection = Quaternion.identity;
-    private Quaternion _compassOrientation = Quaternion.identity;
 
-    void Start()
+    private bool gyroEnabled;
+    private Gyroscope gyro;
+    private GameObject cameraContainer;
+    private Quaternion rot;
+
+    private void Start()
     {
-        Input.gyro.enabled = true;
-        Input.compass.enabled = true;
-        
-
+        cameraContainer = new GameObject("Camera Container");
+        cameraContainer.transform.position = transform.position;
+        transform.SetParent(cameraContainer.transform);
+        gyroEnabled = EnableGyro();
     }
 
-    void Update()
+    private bool EnableGyro()
     {
-        transform.rotation = Input.gyro.attitude;
+        if (SystemInfo.supportsGyroscope)
+        {
+            gyro = Input.gyro;
+            gyro.enabled = true;
+            cameraContainer.transform.rotation = Quaternion.Euler(0f, 90f, 0f);
+            rot = new Quaternion(0, 0, 1, 0);
+            return true;
+        }
+        return false;
+    }
+    private void Update()
+    {
+        if (gyroEnabled)
+        {
+            transform.localRotation = changeAxis(gyro.attitude);
+        }
+    }
+
+    static Quaternion changeAxis(Quaternion q)
+    {
+        var euler = q.eulerAngles;
+        return Quaternion.Euler(-euler.x, -euler.z, -euler.y);
     }
 }
