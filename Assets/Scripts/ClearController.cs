4cbf2403cd973f851fcef85274c733ee9b9ca958
koki <kokik@nangokrstudios.jp>
2021-12-06 23:14:50 +0900

    掃除

--- Assets/Scripts/ClearController.cs
+++ Assets/Scripts/ClearController.cs
@@ -16,7 +16,7 @@ public class ClearController : MonoBehaviour
 
     private IEnumerator Clear()
     {
-        yield return new WaitForSeconds(120f);
+        yield return new WaitForSeconds(10f);
         KeepItManager.Instance.Save(KeepItManager.Instance.Level);
        SceneManager.LoadScene("ClearScene", LoadSceneMode.Additive);
        SceneManager.UnloadSceneAsync("Road");
