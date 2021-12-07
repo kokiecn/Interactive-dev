65e54fa5590bd9e2ebbe107714c3a997e4552b7a
koki <kokik@nangokrstudios.jp>
2021-12-07 22:12:23 +0900

    速さ調整

--- Assets/Scripts/ObstacleDetection.cs
+++ Assets/Scripts/ObstacleDetection.cs
@@ -5,29 +5,21 @@ using UnityEngine;
 public class ObstacleDetection : MonoBehaviour
 {
     [SerializeField]private Camera cam;
-
+    [SerializeField] private GameObject testObj;
+    private Rigidbody rb;
     private void Start()
     {
-     
+        rb = testObj.GetComponent<Rigidbody>();
     }
-    /*
-    // TODOÅ@ëΩï™triggerÇ∂Ç·Ç»Ç¢Ç∆è·äQï®Ç™à¯Ç¡Ç©Ç©ÇÈ
-    private void OnCollisionEnter(Collision collision)
-    {
-        if(collision.gameObject.tag == "Obstacle")
-        {
-            Vibration.Vibrate(200);
-            cam.GetComponent<CameraShake>().Shake(0.2f, 0.01f);
-        }
 
-    }*/
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Obstacle"))
         {
             Vibration.Vibrate(200);
             cam.GetComponent<CameraShake>().Shake(0.2f, 0.01f);
+            rb.AddForce(transform.up *70);
         }
-        Debug.Log("trogger");
+
     }
 }
