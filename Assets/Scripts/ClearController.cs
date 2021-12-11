8de11626d5eb6ec32b5066decaede36b78f7fabb
koki <kokik@nangokrstudios.jp>
2021-12-12 02:33:05 +0900

    残り時間表示

--- Assets/Scripts/ClearController.cs
+++ Assets/Scripts/ClearController.cs
@@ -1,19 +1,33 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 public class ClearController : MonoBehaviour
 {
     [SerializeField] private Material myskybox;
+    [SerializeField] private Text time;
+    public float gameTime = 30.0f;
+    float currentTime;
     private void Awake()
     {
         RenderSettings.skybox = myskybox;
     }
     private void Start()
     {
+        currentTime = gameTime;
         StartCoroutine(Clear());
     }
 
+    private  void Update()
+    {
+        currentTime -= Time.deltaTime;
+        if (currentTime <= 0.0f)
+        {
+            currentTime = 0.0f;
+        }
+        time.text = ((int)currentTime).ToString();
+    }
     private IEnumerator Clear()
     {
         yield return new WaitForSeconds(30f);
